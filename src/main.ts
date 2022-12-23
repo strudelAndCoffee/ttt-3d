da8514b9d55936b79329abe7e1d02a2e447d85e5
Bort <93967399+strudelAndCoffee@users.noreply.github.com>
2022-12-23 01:02:35 -0600

    add winConditions

--- src/main.ts
+++ src/main.ts
@@ -1,2 +1,28 @@
-const el = document.getElementById('main');
-el!.innerText = 'Hi';
+import { winConditions } from './utils/win-conditions';
+
+const X_CLASS = 'x';
+const O_CLASS = 'o';
+
+const plains = document.querySelectorAll('.plain');
+
+let xTurn = true;
+
+plains?.forEach((plain) => {
+  plain.addEventListener('click', (e) =>
+    handleClick(e.target ? e.target : null)
+  );
+});
+
+function handleClick(target: EventTarget | null) {
+  const cell = target as HTMLDivElement;
+  xTurn ? cell.classList.add(X_CLASS) : cell.classList.add(O_CLASS);
+  checkWin();
+}
+
+function checkWin() {
+  winConditions.forEach((combo) => {
+    combo.forEach((coordinate) => {
+      console.log(coordinate);
+    });
+  });
+}
